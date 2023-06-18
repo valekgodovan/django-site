0d4af6a916b1b7a54a52fccb097f62f6ac210af1
valek <valekgodovan@gmail.com>
2023-06-18 22:38:52 +0600

    6th commit

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .models import *
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 # Create your views here.
 def index(request):
-    return HttpResponse('Страница приложения women.')
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'menu': menu, 'title': 'Главная страница', 'posts': posts})
 
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def categories(request, catid):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
