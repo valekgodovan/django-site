e3dfb655a30e3c3892580e525ba7c61959d0cee5
valek <valekgodovan@gmail.com>
2023-06-19 09:55:29 +0600

    9th commit

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -13,10 +13,13 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 # Create your views here.
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         'menu': menu,
         'title': 'Главная страница',
         'posts': posts,
+        'cats': cats,
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -39,6 +42,24 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f'Статья с id = {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404
+
+    context = {
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'posts': posts,
+        'cats': cats,
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
