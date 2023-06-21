4d768c2556c05a69be6d5fb6e48d386058ddf8fd
valek <valekgodovan@gmail.com>
2023-06-21 12:55:10 +0600

    11th commit

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -13,12 +13,10 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 # Create your views here.
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'menu': menu,
         'title': 'Главная страница',
         'posts': posts,
-        'cats': cats,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
@@ -46,7 +44,6 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404
@@ -55,7 +52,6 @@ def show_category(request, cat_id):
         'menu': menu,
         'title': 'Отображение по рубрикам',
         'posts': posts,
-        'cats': cats,
         'cat_selected': cat_id,
     }
 
