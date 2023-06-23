d1f2162ae21e9396cff952fb89bd151dddc92c98
valek <valekgodovan@gmail.com>
2023-06-23 22:25:26 +0600

    12th commit

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from .models import *
@@ -38,12 +38,21 @@ def login(request):
     return HttpResponse('login')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Статья с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'menu': menu,
+        'title': post.title,
+        'post': post,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat__slug=cat_slug)
 
     if len(posts) == 0:
         raise Http404
@@ -52,7 +61,7 @@ def show_category(request, cat_id):
         'menu': menu,
         'title': 'Отображение по рубрикам',
         'posts': posts,
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
 
     return render(request, 'women/index.html', context=context)
