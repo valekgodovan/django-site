d2fe9cbeaa964559c41d2310fb220ade819c4fef
valek <valekgodovan@gmail.com>
2023-07-13 11:57:21 +0700

    19th commit

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
 from .models import *
-from .forms import AddPostForm
+from .forms import *
 from .utils import *
 
 
@@ -136,3 +136,14 @@ class WomenCategory(DataMixin, ListView):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound(f'<h1>Не нашел</h1>')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class =  RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return {**context, **c_def}
